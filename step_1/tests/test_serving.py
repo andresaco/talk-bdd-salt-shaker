969aea0c1fca5f24c686ce53a4c087836e131239
J. Andrés Pizarro <joseandres.pizarro@datiobbva.com>
2024-09-26 10:55:18 +0000

    Fixed Step 1 tests

--- step_1/tests/test_serving.py
+++ step_1/tests/test_serving.py
@@ -5,15 +5,18 @@ from salty import Shaker
 
 scenarios('../features/serving.feature')
 
-@given("A Salt Shaker")
-def salt_shaker():
-    yield Shaker()
-
 @pytest.fixture
-@when("I shake it once")
-def served(salt_shaker):
-    yield salt_shaker.shake()
+def shaker():
+    return Shaker()
+
+@given("A Salt Shaker", target_fixture="shaker")
+def salt_shaker(shaker):
+    return shaker
+
+@when("I shake it once", target_fixture="served_doses")
+def served(shaker):
+    yield shaker.shake()
 
 @then("A salt dose falls on my plate")
-def doses_serve(served):
-    assert served == 1
+def doses_serve(served_doses):
+    assert served_doses == 1
