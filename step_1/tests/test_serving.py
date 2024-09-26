bcc4b668fe0bc841b8f3e44296240df0543ba695
J. Andrés Pizarro <joseandres.pizarro@datiobbva.com>
2024-09-26 11:00:18 +0000

    Fixed linting for step_1

--- step_1/tests/test_serving.py
+++ step_1/tests/test_serving.py
@@ -3,19 +3,23 @@ from pytest_bdd import scenarios, given, when, then
 
 from salty import Shaker
 
-scenarios('../features/serving.feature')
+scenarios("../features/serving.feature")
+
 
 @pytest.fixture
 def shaker():
     return Shaker()
 
+
 @given("A Salt Shaker", target_fixture="shaker")
 def salt_shaker(shaker):
     return shaker
 
+
 @when("I shake it once", target_fixture="served_doses")
 def served(shaker):
     yield shaker.shake()
+
 
 @then("A salt dose falls on my plate")
 def doses_serve(served_doses):
